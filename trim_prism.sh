bfe347213c90070e5b03004df6e3986f6bd5a8a3
ishidamd <yishida@kuhp.kyoto-u.ac.jp>
2022-05-12 13:53:02 +0900

    Implemented proper trimming of UMIs when inserts are short

--- trim_prism.sh
+++ trim_prism.sh
@@ -11,6 +11,10 @@ module load fastp
 READ1=${2}
 READ2=${3}
 
+
+READ1C=`basename ${READ1} .fq.gz`_clipped.fq
+READ2C=`basename ${READ2} .fq.gz`_clipped.fq
+
 READ1T=`basename ${READ1} .fq.gz`_trimmed.fq
 READ2T=`basename ${READ2} .fq.gz`_trimmed.fq
 
@@ -18,7 +22,12 @@ FASTQ_BASE=`basename ${READ1} _1.fq.gz`
 
 mkdir -p ${1}
 
-fastp -i ${READ1} -I ${READ2} -o ${1}/${READ1T} -O ${1}/${READ2T} --detect_adapter_for_pe -f 8 -F 8 --thread 16 -h qc/${FASTQ_BASE}.html
+fastp -i ${READ1} -I ${READ2} -o ${1}/${READ1C} -O ${1}/${READ2C} -f 8 -F 8 --thread 16 -h qc/${FASTQ_BASE}.html --disable_adapter_trimming
+
+fastp -i ${1}/${READ1C} -I ${1}/${READ2C} -o ${1}/${READ1T} -O ${1}/${READ2T} --detect_adapter_for_pe --thread 16 -h qc/${FASTQ_BASE}_2.html
+
+rm ${1}/${READ1C}
+rm ${1}/${READ2C}
 
 pigz -p 16 ${1}/${READ1T}
 pigz -p 16 ${1}/${READ2T}
